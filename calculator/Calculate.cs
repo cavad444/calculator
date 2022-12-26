735497a5b3b5dde4dce58007975fceaebda95d22
rzamirzayev <rzatm@code.edu.az>
2022-12-26 10:54:05 +0400

    Add project files.

---
+++ calculator/Calculate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class Calculator : IOperation
+    {
+        private double _x;
+        private double _y;
+        private string _operator;
+        double netice = 0;
+
+
+        public double X { get { return _x; } set { _x = value; } } 
+        public double Y { get { return _y; } set { _y = value; } } 
+        public string Operator { 
+            get {
+                return _operator; 
+            } 
+            set {
+                try
+                {
+                    _operator = value;
+
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e);
+                }
+            } }
+        public void Calculate()
+        {
+            
+            if(_operator == "+")
+            {
+                netice = _x + _y;
+            }else if (_operator == "*")
+            {
+                netice = _x * _y;
+            }else if(_operator == "/")
+            {
+                netice = _x / _y;
+            }else if (_operator == "-")
+            {
+                netice = _x - _y;
+            }
+            else
+            {
+                Console.WriteLine("Zehmet olmasa bu riyaziyyat emellerinden birini sechin:-,+,/,*");
+            }
+            Console.WriteLine(netice);
+
+           
+            
+        }
+        
+    }
+}
---
+++ calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using calculator;
+
+Console.WriteLine("Bir eded daxil edin");
+double x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Bu emellerden birini daxil edin: *,/,+,-");
+string mathOperator = Console.ReadLine();
+Console.WriteLine("Diger ededi daxil edin");
+double y = Convert.ToInt32(Console.ReadLine());
+Calculator calculate = new();
+calculate.X = x;
+calculate.Y = y;
+calculate.Operator = mathOperator;
+calculate.Calculate();
